9bf7d0b4df68b63b45a0c7a8b08f834adf2c3aad
javisenberg <javier.aranda.designs@gmail.com>
2023-02-10 17:23:46 +0100

    Bump version: 1.1.1 → 1.1.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='edx-analytics-logger',
-    version='1.1.1',
+    version='1.1.2',
     packages=['edx_analytics_logger'],
     url='',
     license='MIT',
