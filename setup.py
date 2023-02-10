cd2172c7442b92c31cee88fbe0ff27a2e3296a43
javisenberg <javier.aranda.designs@gmail.com>
2023-02-10 16:18:41 +0100

    First revision

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='edx-analytics-logger',
+    version='0.0.1',
+    packages=[''],
+    url='',
+    license='MIT',
+    author='javisenberg',
+    description='edx powered ApiBackend'
+)
