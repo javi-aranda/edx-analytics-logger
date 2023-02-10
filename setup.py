ffdc3ad072c29fe9d699128ae49842ba839de31d
javisenberg <javier.aranda.designs@gmail.com>
2023-02-10 17:23:42 +0100

    Fix setup script

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='edx-analytics-logger',
     version='1.1.1',
-    packages=[''],
+    packages=['edx_analytics_logger'],
     url='',
     license='MIT',
     author='javisenberg',
