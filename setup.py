19a1c5bef3bf7abc82e2d19a324bbd993967d0a0
javisenberg <javier.aranda.designs@gmail.com>
2023-08-07 12:32:23 +0200

    Move requirements to separate folder

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 from setuptools import setup
 
+with open('requirements/requirements.txt') as f:
+    install_requires = f.read().splitlines()
+
 setup(
     name='edx-analytics-logger',
     version='1.1.4',
@@ -9,4 +12,5 @@ setup(
     author='javisenberg',
     description='edx powered ApiBackend',
     long_description=open('README.rst').read(),
+    install_requires=install_requires,
 )
